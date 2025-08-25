260f778321c95292b25be869abab62abdf48cff6
danishRajawat <ibnfarooq070@gmail.com>
2025-08-25 23:35:08 +0530

    layout setup and redesign the routes

--- FRONT-END/src/main.jsx
+++ FRONT-END/src/main.jsx
@@ -1,42 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HomePage from "./routes/HomePage.jsx"
-import PostListPage from "./routes/PostListPage.jsx"
-import LoginPage from './routes/LoginPage.jsx'
-import WritePage from './routes/writePage.jsx'
-import RegisterPage from "./routes/RegisterPage.jsx"
-import SinglePostPage from "./routes/SinglePostPage.jsx"
-import {createBrowserRouter , RouterProvider } from "react-router"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import HomePage from "./routes/HomePage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
+import WritePage from "./routes/writePage.jsx";
+import RegisterPage from "./routes/RegisterPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import MainLayout from "./layout/MainLayout.jsx";
 
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : (<HomePage/>)
-  },
-  {
-    path : "/posts",
-    element : (<PostListPage/>)
-  },
-  {
-    path : "/:slug",
-    element : (<SinglePostPage/>)
-  },
-  {
-    path : "/write-post",
-    element : (<WritePage/>)
-  },
-  {
-    path : "/login",
-    element : (<LoginPage/>)
-  },
-  {
-    path : "/register",
-    element : (<RegisterPage/>)
-  },
-])
-createRoot(document.getElementById('root')).render(
+const RouteWrapper = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="" element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/posts" element={<PostListPage />} />
+        <Route path="/:slug" element={<SinglePostPage />} />
+        <Route path="/write-post" element={<WritePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <RouteWrapper />
+  </StrictMode>
+);
